6cc54826f491b21a96e96914bb7e03325d2d0641
callum cunha <callumonthenet@hotmail.com>
2021-03-03 13:29:01 +0000

    -Updated Settings.fxml to style -Added some more style to Settings.fxml

--- Client/src/main/java/Controller/Content/SettingsController.java
+++ Client/src/main/java/Controller/Content/SettingsController.java
@@ -1,11 +1,15 @@
 package Controller.Content;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class SettingsController extends ScrollPane {
+    @FXML VBox container;
+
     /**
      * Constructs the VBox and loads its FXML file.
      */
@@ -20,5 +24,8 @@ public class SettingsController extends ScrollPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        container.prefWidthProperty().bind(this.widthProperty());
+        container.prefHeightProperty().bind(this.heightProperty());
     }
 }
