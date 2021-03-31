05774346652d0f5d8336518b5138488a50ca0551
Nex-Coder <callumonthenet@hotmail.com>
2021-03-31 21:19:44 +0100

    Enable and simplified the event handlers for starting task requests.

--- Client/src/main/java/Controller/Content/GeneralController.java
+++ Client/src/main/java/Controller/Content/GeneralController.java
@@ -5,11 +5,13 @@ import Controller.RootController;
 import Model.Task.Task;
 import Model.Task.TaskRequest;
 import com.jfoenix.controls.JFXButton;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -18,6 +20,20 @@ public class GeneralController extends ScrollPane {
     @FXML JFXButton btnFTPStart;
     @FXML JFXButton btnFTPStop;
     @FXML JFXButton btnFTPRestart;
+
+    @FXML JFXButton btnCleanDiskSystem;
+    @FXML JFXButton btnCleanDiskExtra;
+    @FXML JFXButton btnCleanDiskAll;
+    @FXML JFXButton btnCleanDiskBin;
+
+    @FXML JFXButton btnWIFIEnable;
+    @FXML JFXButton btnWIFIDisable;
+    @FXML JFXButton btnWiFiReenable;
+
+    @FXML JFXButton btnBluetoothEnable;
+    @FXML JFXButton btnBluetoothDisable;
+    @FXML JFXButton btnBluetoothReenable;
+
 
     /**
      * Constructs the VBox and loads its FXML file.
@@ -38,52 +54,40 @@ public class GeneralController extends ScrollPane {
     }
 
     private void applyEventHandlers() {
-        btnFTPStart.setOnMouseClicked(event -> {
-            if (RootController.getTaskService().isRunning()) {
-                Alert alert = RAMTAlert.getAlertMidTask();
-                alert.showAndWait();
+        // FTP Section
+        btnFTPStart.setOnMouseClicked(requestStart(new TaskRequest<>(Task.STARTFTP, RootController.getLoggedInUser())));
+        btnFTPStop.setOnMouseClicked(requestStart(new TaskRequest<>(Task.STOPFTP, RootController.getLoggedInUser())));
+        btnFTPRestart.setOnMouseClicked(requestStart(new TaskRequest<>(Task.RESTARTFTP, RootController.getLoggedInUser())));
 
-                if (alert.getResult() == ButtonType.OK) {
-                    RootController.getTaskService().updateAndRestart(
-                            new TaskRequest<>(Task.STARTFTP, RootController.getLoggedInUser()));
+        // General Section
+        btnCleanDiskSystem.setOnMouseClicked(requestStart(new TaskRequest<>(Task.CLEANDISK, RootController.getLoggedInUser(), 0)));
+        btnCleanDiskExtra.setOnMouseClicked(requestStart(new TaskRequest<>(Task.CLEANDISK, RootController.getLoggedInUser(), 1)));
+        btnCleanDiskAll.setOnMouseClicked(requestStart(new TaskRequest<>(Task.CLEANDISK, RootController.getLoggedInUser(), 2)));
+        btnCleanDiskBin.setOnMouseClicked(requestStart(new TaskRequest<>(Task.CLEANDISK, RootController.getLoggedInUser(), 3)));
 
-                }
-            } else {
-                RootController.getTaskService().updateAndRestart(
-                        new TaskRequest<>(Task.STARTFTP, RootController.getLoggedInUser()));
+        // WIFI Section
+        btnWIFIEnable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.ENABLEWIFI, RootController.getLoggedInUser(), 0)));
+        btnWIFIDisable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.DISABLEWIFI, RootController.getLoggedInUser())));
+        btnWiFiReenable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.ENABLEWIFI, RootController.getLoggedInUser(), 1)));
+
+        // Bluetooth Section
+        btnBluetoothEnable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.ENABLEBLUETOOTH, RootController.getLoggedInUser(), 0)));
+        btnBluetoothDisable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.DISABLEBLUETOOTH, RootController.getLoggedInUser())));
+        btnBluetoothReenable.setOnMouseClicked(requestStart(new TaskRequest<>(Task.ENABLEBLUETOOTH, RootController.getLoggedInUser(), 1)));
+    }
+
+    private EventHandler<? super MouseEvent> requestStart(TaskRequest<?> taskRequest) {
+        if (RootController.getTaskService().isRunning()) {
+            Alert alert = RAMTAlert.getAlertMidTask();
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.OK) {
+                RootController.getTaskService().updateAndRestart(taskRequest);
+
             }
-        });
-
-        btnFTPStop.setOnMouseClicked(event -> {
-            if (RootController.getTaskService().isRunning()) {
-                Alert alert = RAMTAlert.getAlertMidTask();
-                alert.showAndWait();
-
-                if (alert.getResult() == ButtonType.OK) {
-                    RootController.getTaskService().updateAndRestart(
-                            new TaskRequest<>(Task.STOPFTP, RootController.getLoggedInUser()));
-
-                }
-            } else {
-                RootController.getTaskService().updateAndRestart(
-                        new TaskRequest<>(Task.STOPFTP, RootController.getLoggedInUser()));
-            }
-        });
-
-        btnFTPRestart.setOnMouseClicked(event -> {
-            if (RootController.getTaskService().isRunning()) {
-                Alert alert = RAMTAlert.getAlertMidTask();
-                alert.showAndWait();
-
-                if (alert.getResult() == ButtonType.OK) {
-                    RootController.getTaskService().updateAndRestart(
-                            new TaskRequest<>(Task.RESTARTFTP, RootController.getLoggedInUser()));
-
-                }
-            } else {
-                RootController.getTaskService().updateAndRestart(
-                        new TaskRequest<>(Task.RESTARTFTP, RootController.getLoggedInUser()));
-            }
-        });
+        } else {
+            RootController.getTaskService().updateAndRestart(taskRequest);
+        }
+        return null;
     }
 }
