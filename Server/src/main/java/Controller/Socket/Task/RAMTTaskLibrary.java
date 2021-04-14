0dd6cf731d5f19d9fb6dee415707dc3943a5aca7
Nex-Coder <callumonthenet@hotmail.com>
2021-04-14 05:43:27 +0100

    Hopefully fixed MacOS bluetooth operations

--- Server/src/main/java/Controller/Socket/Task/RAMTTaskLibrary.java
+++ Server/src/main/java/Controller/Socket/Task/RAMTTaskLibrary.java
@@ -890,7 +890,7 @@ public class RAMTTaskLibrary {
 
                 case MAC:
                     if (request.getParameter() == 1) { disableBluetooth(request); } // Re-enable
-                    Process macCMD = new ProcessBuilder("/bin/zsh",  "-e", "launchctl start com.apple.blued").start();
+                    Process macCMD = new ProcessBuilder("/bin/zsh",  "-c", "launchctl start com.apple.blued").start();
 
                     return macCMD.waitFor() != 0 ?
                             new TaskResponse<>(request, Response.FAILED, 1) :
@@ -943,11 +943,12 @@ public class RAMTTaskLibrary {
                             new TaskResponse<>(request, Response.SUCCESS, 0);
 
                 case MAC:
-                    Process macCMD = new ProcessBuilder("/bin/zsh",  "-e", "launchctl stop com.apple.blued").start();
+                    Process macCMD = new ProcessBuilder("/bin/zsh",  "-c", "launchctl stop com.apple.blued").start();
 
                     return macCMD.waitFor() != 0 ?
                             new TaskResponse<>(request, Response.FAILED, 1) :
                             new TaskResponse<>(request, Response.SUCCESS, 0);
+
                 default:
                     return new TaskResponse<>(request, Response.FAILED, 98); // Unsupported
             }
