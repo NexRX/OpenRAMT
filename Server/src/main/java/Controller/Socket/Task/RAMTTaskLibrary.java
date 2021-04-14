6586257e30a8172a8074d45e93a3192974d8a3f4
Nex-Coder <callumonthenet@hotmail.com>
2021-04-14 05:15:05 +0100

    Hopefully fixed MacOS enable wifi

--- Server/src/main/java/Controller/Socket/Task/RAMTTaskLibrary.java
+++ Server/src/main/java/Controller/Socket/Task/RAMTTaskLibrary.java
@@ -890,7 +890,7 @@ public class RAMTTaskLibrary {
 
                 case MAC:
                     if (request.getParameter() == 1) { disableBluetooth(request); } // Re-enable
-                    Process macCMD = new ProcessBuilder("osascript",  "-e", scriptMacOSBluetooth(true)).start();
+                    Process macCMD = new ProcessBuilder("/bin/zsh",  "-c", scriptMacOSBluetooth(false)).start();
 
                     return macCMD.waitFor() != 0 ?
                             new TaskResponse<>(request, Response.FAILED, 1) :
